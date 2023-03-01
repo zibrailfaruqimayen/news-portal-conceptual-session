cc2f1e41d91ce0710bbb4ee4b2f6a3907278975d
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-01 17:02:14 +0600

    first commit

---
+++ scripts/news-portal.js
@@ -0,0 +1,6 @@
+const fetchCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/news/categories")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
+// fetchCategories();
