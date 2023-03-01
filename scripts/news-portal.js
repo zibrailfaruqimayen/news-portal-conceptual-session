b37109b9597f51b34d9342961c0452e58b3be3d0
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-01 17:56:12 +0600

    news category done

--- scripts/news-portal.js
+++ scripts/news-portal.js
@@ -1,6 +1,25 @@
 const fetchCategories = () => {
   fetch("https://openapi.programming-hero.com/api/news/categories")
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => showCategories(data.data));
 };
 // fetchCategories();
+
+const showCategories = (data) => {
+  //   console.log(data);
+  //capture Categories container to append all the category links
+  const categoriesContainer = document.getElementById("categories-container");
+  data.news_category.forEach((singleCategories) => {
+    // console.log(singleCategories);
+    // if it seems difficult, we can skip it step: 1 (advanced)
+    categoriesContainer.innerHTML += `<a class="nav-link" href="#">${singleCategories?.category_name}</a>`;
+
+    // step: 2 (recommended for all of us)
+
+    // const linkContainer = document.createElement("p");
+    // linkContainer.innerHTML = `<a class="nav-link" href="#">${singleCategories.category_name}</a>`;
+    // categoriesContainer.appendChild(linkContainer);
+  });
+};
+
+// fetch all newses available in a category
