3d0e5335cacdbd5b3c99bebc7d6f205c98641bdb
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-01 20:32:38 +0600

    alrte category done

--- scripts/news-portal.js
+++ scripts/news-portal.js
@@ -9,10 +9,10 @@ const showCategories = (data) => {
   //   console.log(data);
   //capture Categories container to append all the category links
   const categoriesContainer = document.getElementById("categories-container");
-  data.news_category.forEach((singleCategories) => {
+  data.news_category.forEach((singleCategory) => {
     // console.log(singleCategories);
     // if it seems difficult, we can skip it step: 1 (advanced)
-    categoriesContainer.innerHTML += `<a class="nav-link" href="#">${singleCategories?.category_name}</a>`;
+    categoriesContainer.innerHTML += `<a class="nav-link" href="#" onclick="fetchCategoryNews('${singleCategory.category_id}', '${singleCategory?.category_name}')">${singleCategory?.category_name}</a>`;
 
     // step: 2 (recommended for all of us)
 
@@ -23,3 +23,16 @@ const showCategories = (data) => {
 };
 
 // fetch all newses available in a category
+const fetchCategoryNews = (category_id, category_name) => {
+  // console.log(category_id);
+  const url = ` https://openapi.programming-hero.com/api/news/category/${category_id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showAllNews(data.data, category_name));
+};
+
+const showAllNews = (data, category_name) => {
+  console.log(data, category_name);
+  document.getElementById("news-count").innerText = data.length;
+  document.getElementById("category-name").innerText = category_name;
+};
